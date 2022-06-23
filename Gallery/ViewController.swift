6732dd9cdfe91b645600cb61765a2732135f56a8
bullinnyc <i8u11@icloud.com>
2022-06-23 19:33:25 +0700

    Initial Commit

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Dmitry Kononchuk on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
